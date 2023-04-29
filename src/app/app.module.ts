db73d7c78a2599f8d3ce169a8659cbc40e5e6d2f
shixismayil <sixismayil@gmail.com>
2023-04-29 10:51:29 +0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeaturesComponent } from './features/features.component';
+import { IconGridComponent } from './icon-grid/icon-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    ProductsComponent,
+    FooterComponent,
+    FeaturesComponent,
+    IconGridComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/icon-grid/icon-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-grid',
+  templateUrl: './icon-grid.component.html',
+  styleUrls: ['./icon-grid.component.scss']
+})
+export class IconGridComponent {
+
+}
